7a05cdd2f8d18404a48fe8e9d1c97faae089f04a
mejszin <louis@formka.com>
2020-07-14 01:37:11 -0700

    Amended path name

--- files/watch.sh
+++ files/watch.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
 while true; do
-#ls -d /home/ftpuser/ftp/* |entr -d ./first.sh
-find /home/ftpuser/ftp/ -type f -name "*.txt" | entr -d ./first.sh
+find /home/ftpuser/ftp/ -type f -name "*.txt" | entr -d ./trigger.sh
 done
