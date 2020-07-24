1b44772376f59074669c047782a1f39d51cef81a
ldhmachin <ldhmachin@gmail.com>
2020-07-24 08:24:03 +0100

    Changed watch script

--- sh/watch.sh
+++ sh/watch.sh
@@ -3,5 +3,5 @@
 echo "Starting directory watcher..."
 
 while true; do
-find /home/ftpuser/ftp/ -type f -name "*.txt" | entr -p ./rb/scripts/import_ftp.rb > /dev/null
+find /home/ftpuser/ftp/ -type f -name "*.txt" | entr -p ./rb/scripts/import_ftp.rb
 done
