8d55aa42a580d30bc7e25c6951dedfe84b70e5df
ldhmachin <ldhmachin@gmail.com>
2020-07-24 08:58:40 +0100

    Changed ruby script

--- rb/scripts/import_ftp.rb
+++ rb/scripts/import_ftp.rb
@@ -6,7 +6,7 @@ dir_path = "/home/ftpuser/ftp/"
 
 Dir.foreach(dir_path) do |file_path|
     unless file_path == '.' || file_path == '..'
-        if ['.txt'].include?(File.extname(file_path)
+        if ['.txt'].include?(File.extname(file_path))
             full_path = File.join(dir_path, file_path)
             lines = File.readlines(full_path)
             puts lines.inspect
