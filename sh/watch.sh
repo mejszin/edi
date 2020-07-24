5a7b2c1e3bee88413d5f61f0fdcd37ec8e2ebdf8
ldhmachin <ldhmachin@gmail.com>
2020-07-24 07:07:18 +0100

    Amended watch script

--- sh/watch.sh
+++ sh/watch.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
 while true; do
-ruby ./rb/scripts/import_ftp.rb | entr -d ./trigger.sh
+
+#ruby ./rb/scripts/import_ftp.rb | entr -d ./trigger.sh
 
 #find /home/ftpuser/ftp/ -type f -name "*.txt" | entr -d ./trigger.sh
+find /home/ftpuser/ftp/ -type f -name "*.txt" | entr -d ./rb/scripts/import_ftp.rb
+
 done
