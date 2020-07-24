5f97aeee53247e56e489fb975332209995f82189
ldhmachin <ldhmachin@gmail.com>
2020-07-24 08:02:10 +0100

    Amended import scripts

--- rb/scripts/import_ftp.rb
+++ rb/scripts/import_ftp.rb
@@ -1,10 +1,9 @@
 #!/usr/bin/ruby
 
 puts "HELLO WORLD!!! - Ruby"
-puts ARGV.inspect
 
-#dir_path "/home/ftpuser/ftp/"
-#Dir.foreach(dir_path) do |f|
-#    fn = File.join(dir_path, f)
-#    File.delete(fn) if f != '.' && f != '..' && "entr.txt"
-#end
+dir_path "/home/ftpuser/ftp/"
+Dir.foreach(dir_path) do |f|
+    fn = File.join(dir_path, f)
+    File.delete(fn) if f != '.' && f != '..' && "entr.txt"
+end
--- sh/watch.sh
+++ sh/watch.sh
@@ -3,5 +3,5 @@
 echo "Starting directory watcher..."
 
 while true; do
-find /home/ftpuser/ftp/ -type f -name "*.txt" | entr -p ./rb/scripts/import_ftp.rb
+find /home/ftpuser/ftp/ -type f -name "*.txt" | entr -d ./rb/scripts/import_ftp.rb
 done
