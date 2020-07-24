3391922bf64bdda571eb359a937e6fa30eaf48fb
ldhmachin <ldhmachin@gmail.com>
2020-07-24 07:56:39 +0100

    Amended test script

--- rb/scripts/import_ftp.rb
+++ rb/scripts/import_ftp.rb
@@ -2,3 +2,9 @@
 
 puts "HELLO WORLD!!! - Ruby"
 puts ARGV.inspect
+
+#dir_path "/home/ftpuser/ftp/"
+#Dir.foreach(dir_path) do |f|
+#    fn = File.join(dir_path, f)
+#    File.delete(fn) if f != '.' && f != '..' && "entr.txt"
+#end
