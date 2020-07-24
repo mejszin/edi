96f03424f127cab65cb0298528539d5b248cbf85
ldhmachin <ldhmachin@gmail.com>
2020-07-24 08:57:09 +0100

    Changed ruby script

--- rb/scripts/import_ftp.rb
+++ rb/scripts/import_ftp.rb
@@ -3,7 +3,14 @@
 puts "HELLO WORLD!!! - Ruby"
 
 dir_path = "/home/ftpuser/ftp/"
-Dir.foreach(dir_path) do |f|
-    fn = File.join(dir_path, f)
-    File.delete(fn) if f != '.' && f != '..' && f != "entr.txt"
+
+Dir.foreach(dir_path) do |file_path|
+    unless file_path == '.' || file_path == '..'
+        if ['.txt'].include?(File.extname(file_path)
+            full_path = File.join(dir_path, file_path)
+            lines = File.readlines(full_path)
+            puts lines.inspect
+            File.delete(full_path)
+        end
+    end
 end
