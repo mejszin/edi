b2111a6cadd0ce9f98d7a1edc153e0f4e826cb2b
ldhmachin <ldhmachin@gmail.com>
2020-07-24 08:04:02 +0100

    Fixed assignment error

--- rb/scripts/import_ftp.rb
+++ rb/scripts/import_ftp.rb
@@ -5,5 +5,5 @@ puts "HELLO WORLD!!! - Ruby"
 dir_path = "/home/ftpuser/ftp/"
 Dir.foreach(dir_path) do |f|
     fn = File.join(dir_path, f)
-    File.delete(fn) if f != '.' && f != '..' && "entr.txt"
+    File.delete(fn) if f != '.' && f != '..' && f != "entr.txt"
 end
