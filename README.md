257a4107896e2199142a681419755ab7f0869ffc
ldhmachin <ldhmachin@gmail.com>
2020-07-24 07:45:04 +0100

    Added readme information

---
+++ README.md
@@ -0,0 +1,6 @@
+## Running FTP watch script
+Create new TMUX session and run watch shell script
+``tmux
+``./sh/watch.sh
+You can close the session and reattach to end the script
+``tmux attach
--- sh/watch.sh
+++ sh/watch.sh
@@ -5,6 +5,6 @@ while true; do
 #ruby ./rb/scripts/import_ftp.rb | entr -d ./trigger.sh
 
 #find /home/ftpuser/ftp/ -type f -name "*.txt" | entr -d ./trigger.sh
-find /home/ftpuser/ftp/ -type f -name "*.txt" | entr -d ./rb/scripts/import_ftp.rb
+find /home/ftpuser/ftp/ -type f -name "*.txt" | entr -p ./rb/scripts/import_ftp.rb
 
 done
